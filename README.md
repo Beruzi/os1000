cc50b68f66aac9397b463ef233ddb5087dcc5a00
Michael Taylor <247michaeltaylor@gmail.com>
2025-10-01 02:07:59 -0500

    Cleaned up comments, updated readme, working on openSBI implementatio/use

--- README.md
+++ README.md
@@ -4,11 +4,24 @@ Some notes for myself....
 - Once executing, use ctrl + a to escape into QEMU debugging (QEMU monitor). I think this is only possible because we configure QEMU to route its standard input and output to the machine's serial port? 
 - After producing a `.elf` file, you can `$llvm-objdump` to see the assembly created. This is the `.text` segment, ie the actual instructions (duh)
 
-
-
+Examining low level stuff:
+- `llvm-objdump` to see assembly of .text
+- `llvm-nm file.elf` to see the symbol table
 
 
 # Summary & Notable Parts
+
+### SBI & OpenSBI
+Supervisor Binary Interfac (SBI) is an API for OS kernels. Recall that kernels CPUs run in different modes... in RISCV, this includes:
+- M-mode - machine mode, highest privillege, used by bootloader/firmware
+- S-mode - supervisor mode... aka just kernel mode. For OS
+- U-mode - user mode
+
+Based on our QEMU flags, we use openSBI (an open source API for SBI) to actually enable us to switch between modes. 
+
+Alongisde this, openSBI provides the `ecall` instruction... this is analgous to syscalls. 
+- Process invokves syscall to OS to do something on the process's behalf that it doesn't have permissions for
+- OS invokes ecall to openSBI to do something on the OS's behalf that it doesn't have permissions for. 
 
 ### Linker File (.ld)
 The `.ld` file is used to specify any sort explicit linking configurations that you want your project to compile with. In this case, we use the `.ld` file to manually layout the memory of our OS rather than let the compiler handle it for us. More specifically, we define the OS's memory to be a *contiguous*, logically segmented memory space... so nothing fancy like base & bounds or paging... yet.
--- kernel.c
+++ kernel.c
@@ -1,3 +1,4 @@
+
 // Aliasing some types
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
@@ -7,65 +8,38 @@ typedef uint32_t size_t;
 extern char __bss[], __bss_end[], __stack_top[];
 
 
-/* 
- * Getting into some C stuff... 
- * - C does not have templates, so in order to generalize types we 
- *   return or pass in void*. These are pointers to some address with 
- *   an unknown type. Can later cast the pointer into the proper type
- */
-
-
+// Fill Memory
 void* memset(void* buf, char c, size_t n) {
-	// Cast pointer of buffer to uint8_t type... this allows for 
-	// arithmetic to be performed. Since the pointer is 8bits, incrementation goes by byte
 	uint8_t* p = (uint8_t* )buf;
 
-	// dereference and assign the pointer p to the value c n times.
 	while(n--) { 
 		*p = c;
 		++p;
-		// could combine lines into *p++ = c;
-		// Order of OP: postincrement, dereference (before increment) assign to c;
+		// *p++ = c is the same as both lines above
 	}
 	return buf;
 }
 
 
 void kernel_main(void) {
-	// calling the memset functino above using the 
+	// 0 out the .bss section using memset()
 	memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-	for(;;); // infinte loop
+	for(;;); // infinte loop; hence why after ./run.sh, nothing happens
 }
 
-/* Some compiler extensions... 
- * __attribute__(<something>) --> tells the of a special attritbute or
- * configuration you want to apply to something
- *
- *__asm__ __volatile__ --> this is inline assembly code 
- *
-*/
 
-// Here we explictly tell the compiler what "named" section we want
-// to store data in based on the .ld section (.text.tboot)
-// Recall that in .ld we explictly used KEEP() to include the boot() 
-// function in our .text section
-//
-// (naked) is used to tell the compiler to not generate any extra 
-// instructions, guaranteeing that the boot() function contains only
-// the specified assembly
-__attribute__((section(".text.tboot")))
+// Compiler Directives
+// 1. Force the "boot" function into the .text.boot subsection
+// 2. Don't add any extra asm... this will avoid the usual function prolouge and epilouge... both of which attempt to use the SP.
+__attribute__((section(".text.boot")))
 __attribute__((naked))
 void boot(void) {
 	__asm__ __volatile__(
-		"mv sp, %[stack_top]\n" // raw asm to set stack pointer (sp)
+		"mv sp, %[stack_top]\n"	// raw asm to load stack top address to sp reg 
 		"j kernel_main\n"		// Jump to kernel main function
 		:
-		: [stack_top] "r" (__stack_top) // Pass stack address  
+		: [stack_top] "r" (__stack_top)
 	);
 }
-
-
-
-
 
